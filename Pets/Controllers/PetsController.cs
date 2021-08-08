fb0bbf58feceeaea18672a7fcc903c497db6d0d8
Derick Beckwith <derick.beckwith@gmail.com>
2021-08-08 10:48:25 -0400

    add the Pets controller class

---
+++ Pets/Controllers/PetsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pets.Interfaces;
+
+namespace Pets.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PetsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<IPet> Get()
+        {
+            return new List<IPet>();
+        }
+    }
+}
