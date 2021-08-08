f33de2f34aacbdf76e7d5110140a156d749c26e4
Derick Beckwith <derick.beckwith@gmail.com>
2021-08-08 10:42:08 -0400

    add the IPet interface

---
+++ Pets/Interfaces/IPet.cs
@@ -0,0 +1,35 @@
+﻿namespace Pets.Interfaces
+{
+    public interface IPet
+    {
+        /// <summary>
+        /// The pet's unique identifier.
+        /// </summary>
+        int Id { get; }
+
+        /// <summary>
+        /// The name of the pet.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// The name of the pet's species.
+        /// </summary>
+        string Species { get; }
+
+        /// <summary>
+        /// Male or Female.
+        /// </summary>
+        string Sex { get; }
+
+        /// <summary>
+        /// Age in months.
+        /// </summary>
+        int Age { get; }
+
+        /// <summary>
+        /// The name of the pet's breed.
+        /// </summary>
+        string Breed { get; }
+    }
+}
