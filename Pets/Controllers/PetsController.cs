ff14be408f1fa2003de86eb2858a0f644297e1d4
Derick Beckwith <derick.beckwith@gmail.com>
2021-08-10 01:40:39 -0400

    add routes for update and delete pets

--- Pets/Controllers/PetsController.cs
+++ Pets/Controllers/PetsController.cs
@@ -19,16 +19,16 @@ namespace Pets.Controllers
             {
                 _idCount = 0;
 
-                var dog = new Pet(++_idCount, "Fido", "Dog", "Male", 24, "Labrador");
-                var cat = new Pet(++_idCount, "Skraggles", "Cat", "Female", 11, "Calico");
+                var dog = new Pet(++_idCount, "Delilah", "Dog", "Female", 96, "Chocolate Lab");
+                var cat = new Pet(++_idCount, "Skraggles", "Cat", "Male", 22, "Orange Tabby");
 
                 _pets = new List<IPet> { dog, cat };
             }
         }
 
         /// <summary>
-        /// Get all pets.
-        /// GET: api/Pets
+        /// Find all pets.
+        /// GET: api/pets
         /// </summary>
         [HttpGet]
         public ActionResult<IEnumerable<IPet>> GetPets()
@@ -37,42 +37,86 @@ namespace Pets.Controllers
         }
 
         /// <summary>
-        /// Get the pet with the given id.
-        /// GET: api/Pets/5
+        /// Get a pet by an id.
+        /// GET: api/pets/5
         /// </summary>
         [HttpGet("{id}")]
         public ActionResult<IPet> GetPet(int id)
         {
-            IPet found = null;
-
             foreach (var pet in _pets)
             {
                 if (pet.Id == id)
                 {
-                    found = pet;
+                    return new ActionResult<IPet>(pet);
                 }
             }
 
-            if (found is null)
-            {
-                return NotFound();
-            }
-
-            return new ActionResult<IPet>(found);
+            return NotFound();
         }
 
         /// <summary>
-        /// Creates a new pet.
-        /// POST: api/Pets
+        /// Create a pet.
+        /// POST: api/pets
         /// </summary>
         [HttpPost]
         public ActionResult<IPet> PostPet(Pet pet)
         {
-            var newPet = new Pet(++_idCount, pet.Name, pet.Species, pet.Sex, pet.Age, pet.Breed);
+            var p = new Pet(++_idCount, pet.Name, pet.Species, pet.Sex, pet.Age, pet.Breed);
 
-            _pets.Add(newPet);
+            _pets.Add(p);
 
-            return CreatedAtAction(nameof(GetPet), new { id = newPet.Id }, newPet);
+            return CreatedAtAction(nameof(GetPet), new { id = p.Id }, p);
+        }
+
+        /// <summary>
+        /// Update a pet by an Id.
+        /// PUT: api/pets/5
+        /// </summary>
+        [HttpPut("{id}")]
+        public IActionResult PutPet(long id, Pet pet)
+        {
+            if (id != pet.Id)
+            {
+                return BadRequest();
+            }
+
+            foreach (var p in _pets)
+            {
+                if (p.Id == id)
+                {
+                    // Update the pet in place.
+                    p.Name = pet.Name;
+                    p.Species = pet.Species;
+                    p.Sex = pet.Sex;
+                    p.Age = pet.Age;
+                    p.Breed = pet.Breed;
+
+                    return NoContent();
+                }
+            }
+
+            return NotFound();
+        }
+
+        /// <summary>
+        /// Delete a pet by an Id.
+        /// DELETE: api/pets/5
+        /// </summary>
+        [HttpDelete("{id}")]
+        public IActionResult DeletePet(long id)
+        {
+            foreach (var pet in _pets)
+            {
+                if (pet.Id == id)
+                {
+                    // Remove the pet from the list.
+                    _pets.Remove(pet);
+
+                    return NoContent();
+                }
+            }
+
+            return NotFound();
         }
     }
 }
